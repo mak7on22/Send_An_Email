ceabf87d293a418276ce93097af2de9d0d44a6a8
mak7on22 <mur-mur-0998@mail.ru>
2023-11-24 01:08:50 +0600

    added the default previous data from the td sheet application

---
+++ TDList/Sorts/TDLSortState.cs
@@ -0,0 +1,17 @@
+﻿namespace TDList.Sorts
+{
+        public enum TDLSortState
+        {
+            NameAsc,
+            NameDesc,
+            PriorityValueAsc,
+            PriorityValueDesc,
+            StatusValueAsc,
+            StatusValueDesc,
+            CreatedAsc,
+            CreatedDesc
+    }
+    
+
+}
+
---
+++ TDList/wwwroot/js/site.js
@@ -0,0 +1,34 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+$(document).ready(function () {
+    $('.priority-label').each(function () {
+        var priority = $(this).data('priority');
+        switch (priority) {
+            case 'High':
+                $(this).addClass('high-priority');
+                break;
+            case 'Medium':
+                $(this).addClass('medium-priority');
+                break;
+            case 'Low':
+                $(this).addClass('low-priority');
+                break;
+            default:
+                break;
+        }
+    });
+});
+
+$(document).ready(function () {
+    $(".accordion").click(function () {
+        var panel = $(this).next();
+        if (panel.css("display") === "block") {
+            panel.css("display", "none");
+        } else {
+            panel.css("display", "block");
+        }
+    });
+});
+
