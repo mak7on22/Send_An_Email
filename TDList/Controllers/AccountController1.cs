b778dfbc989cea3f7489bf807f1ce8f5215893b1
mak7on22 <mur-mur-0998@mail.ru>
2023-11-24 01:05:47 +0600

    added verification via email the code is also sent via the user's confirmation email with the email is also sent via the email service

---
+++ TDList/Controllers/AccountController1.cs
@@ -0,0 +1,178 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Drawing;
+using TDList.Models;
+using TDList.Servises;
+using TDList.ViewModels;
+
+namespace TDList.Controllers
+{
+        [ResponseCache(CacheProfileName = "Cashing")]
+        public class AccountController : Controller
+        {
+            private readonly TDLContext _context;
+            private readonly UserManager<User> _userManager;
+            private readonly SignInManager<User> _signInManager;
+            private readonly IEmailSendler _emailSendler;
+            public AccountController(UserManager<User> userManager, IEmailSendler emailSendler, SignInManager<User> signInManager, TDLContext context)
+            {
+                _userManager = userManager;
+                _signInManager = signInManager;
+                _context = context;
+                _emailSendler = emailSendler;
+            }
+            [HttpGet]
+            public IActionResult Register()
+            {
+                return View();
+            }
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var existingUserLogin = await _userManager.FindByNameAsync(model.Login);
+                var existingUserEmail = await _userManager.FindByEmailAsync(model.Email);
+
+                if (existingUserLogin != null)
+                    ModelState.AddModelError(string.Empty, "Пользователь с таким Login уже существует.");
+                if (existingUserEmail != null)
+                    ModelState.AddModelError(string.Empty, "Пользователь с таким Email уже существует.");
+                if (existingUserLogin != null || existingUserEmail != null)
+                    return View(model);
+                User user = new User
+                {
+                    Login = model.Login,
+                    Email = model.Email,
+                    UserName = model.Login,
+                    RoleName = "Member"
+                };
+                IdentityResult result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    Random random = new();
+                    int confirmationCode = random.Next(1000, 10000);
+                    string codeAsString = confirmationCode.ToString();
+                    char[] codeArray = codeAsString.ToCharArray();
+                    char digit1 = codeArray[0];
+                    char digit2 = codeArray[1];
+                    char digit3 = codeArray[2];
+                    char digit4 = codeArray[3];
+                    string message = $"<div style=\"background-color: #181530; border-radius: 20px; padding: 20px; display: flex; flex-direction: column; align-items: center;\"><div style=\"color: white;font-size: x-large;font-weight: 700;\">Приветствуем вас!</div></br><div style=\"color: white;font-weight: 600;font-size: large;\">Для завершения регистрации введите следующий код подтверждения в поле на сайте</div></br><div style=\"display: flex;color: white;gap: 15px;font-size: x-large;font-weight: 800;\"><p style=\"border: 2px solid #3a1a3c;padding: 10px; border-radius: 10px; background-color: #0b070b;\">{digit1}</p><p style=\"border: 2px solid #3a1a3c;padding: 10px; border-radius: 10px; background-color: #0b070b;\">{digit2}</p><p style=\"border: 2px solid #3a1a3c;padding: 10px; border-radius: 10px; background-color: #0b070b;\">{digit3}</p><p style=\"border: 2px solid #3a1a3c;padding: 10px; border-radius: 10px; background-color: #0b070b;\">{digit4}</p></div></br><div style=\"color:white; font-size: x-large; font-weight: 600;\">Спасибо за выбор нашего сервиса!</div></div>";
+                    string subject = "Подтверждение регистрации на сайте";
+                    TempData["ConfirmationCode"] = confirmationCode;
+                    await _emailSendler.SendEmailAsync(model.Email, subject, message);
+                    return RedirectToAction("ConfirmRegistration", new { email = model.Email });
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                        ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+            return View(model);
+        }
+		public IActionResult Werifications(int? id)
+		{
+			User user = _context.Users.FirstOrDefault(u => u.Id == id)!;
+			if (user != null)
+			{
+				string userEmail = user.Email;
+				return RedirectToAction("ConfirmRegistration", new { email = userEmail });
+			}
+			else
+				return RedirectToAction("Error");
+		}
+		[HttpGet]
+        public IActionResult ConfirmRegistration(string email)
+        {
+            ViewData["Email"] = email;
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ConfirmRegistration(string email, int confirmationCode)
+        {
+            int storedCode = (int)TempData["ConfirmationCode"]!;
+            if (confirmationCode == storedCode)
+            {
+                var user = await _userManager.FindByEmailAsync(email);
+                if (user != null)
+                {
+                    user.EmailConfirmed = true;
+                    user.IsConfirmed = true;
+                    await _userManager.UpdateAsync(user);
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("Index", "Goals");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Пользователь не найден.");
+                    return View();
+                }
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Неверный код подтверждения. Пожалуйста, повторите попытку.");
+                return View();
+            }
+        }
+        [HttpGet]
+            public IActionResult Login(string returnUrl = null!)
+            {
+                return View(new LoginViewModel { ReturnUrl = returnUrl });
+            }
+            [HttpPost]
+            [ValidateAntiForgeryToken]
+            public async Task<IActionResult> Login(LoginViewModel model)
+            {
+                if (ModelState.IsValid)
+                {
+                    User user = null!;
+                    if (!string.IsNullOrEmpty(model.Email))
+                    {
+                        if (!model.Email.Contains('@'))
+                            user = await _userManager.FindByNameAsync(model.Email);
+                        else
+                            user = await _userManager.FindByEmailAsync(model.Email);
+                    }
+                    else if (!string.IsNullOrEmpty(model.Login))
+                        user = await _userManager.FindByNameAsync(model.Login);
+                    if (user != null)
+                    {
+                        Microsoft.AspNetCore.Identity.SignInResult result = await _signInManager.PasswordSignInAsync(
+                            user,
+                            model.Password,
+                            model.RememberMe,
+                            false
+                        );
+                        if (result.Succeeded)
+                        {
+                            if (!string.IsNullOrEmpty(model.ReturnUrl) && Url.IsLocalUrl(model.ReturnUrl))
+                                return Redirect(model.ReturnUrl);
+                            else
+                                return RedirectToAction("Index", "Goals");
+                        }
+                    }
+
+                    ModelState.AddModelError("", "Неправильный логин или пароль");
+                }
+                return View(model);
+            }
+            [HttpPost]
+            [ValidateAntiForgeryToken]
+            public async Task<IActionResult> LogOff()
+            {
+                await _signInManager.SignOutAsync();
+                return RedirectToAction("Index", "Goals");
+            }
+            [HttpGet]
+            [Authorize(Roles = "MainAdmin,Manager")]
+            public IActionResult CreateUser()
+            {
+                return View();
+            }
+        }
+}
---
+++ TDList/Controllers/GoalsController.cs
@@ -0,0 +1,278 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TDList.Models;
+using TDList.Sorts;
+using TDList.Enums;
+using System.Globalization;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.Extensions.Localization;
+using Microsoft.AspNetCore.Authorization;
+
+namespace TDList.Controllers
+{
+    public class GoalsController : Controller
+    {
+        private readonly TDLContext _context;
+        private readonly IStringLocalizer<GoalsController> _localizer;
+
+        public GoalsController(TDLContext context, IStringLocalizer<GoalsController> localizer)
+        {
+            _context = context;
+            _localizer = localizer;
+        }
+
+        public async Task<IActionResult> Index(string searchWords, DateTime? startDate, DateTime? endDate, Priority? pr, string? st, int pg = 1, TDLSortState sortState = TDLSortState.NameAsc)
+        {
+            const int pageSize = 10;
+            if (pg < 1)
+                pg = 1;
+            IQueryable<Goal> tdlContext = _context.Goals;
+            if (searchWords != null)
+                tdlContext = tdlContext.Where(x => x.Name == searchWords || x.Description.Contains(searchWords));            
+            if (startDate != null)
+                tdlContext = tdlContext.Where(x => x.Created >= startDate.Value);
+            if (endDate != null) 
+                tdlContext = tdlContext.Where(x => x.Created <= endDate.Value);
+            if (pr != null) 
+                tdlContext = tdlContext.Where(x => x.Priority == pr);
+            if (st != null && st != "All") 
+                tdlContext = tdlContext.Where(x => x.Status == st);
+
+            var goalsCircle = await _context.Goals.ToListAsync();
+            foreach (var g in goalsCircle)
+            {
+                var priority = g.Priority;
+                var status = g.Status;
+                if (priority == Enums.Priority.Low) g.PriorityValue = 1;
+                else if (priority == Enums.Priority.Medium) g.PriorityValue = 2;
+                else g.PriorityValue = 3;
+                if (status == "Новая") g.StatusValue = 1;
+                else if (status == "В процессе") g.StatusValue = 2;
+                else g.StatusValue = 3;
+            }
+            await _context.SaveChangesAsync();
+            switch (sortState)
+            {
+                case TDLSortState.NameAsc: tdlContext = tdlContext.OrderBy(p => p.Name); break;
+                case TDLSortState.PriorityValueAsc: tdlContext = tdlContext.OrderBy(p => p.PriorityValue); break;
+                case TDLSortState.StatusValueAsc: tdlContext = tdlContext.OrderBy(p => p.StatusValue); break;
+                case TDLSortState.CreatedAsc: tdlContext = tdlContext.OrderBy(p => p.Created); break;
+                case TDLSortState.NameDesc: tdlContext = tdlContext.OrderByDescending(p => p.Name); break;
+                case TDLSortState.PriorityValueDesc: tdlContext = tdlContext.OrderByDescending(p => p.PriorityValue); break;
+                case TDLSortState.StatusValueDesc: tdlContext = tdlContext.OrderByDescending(p => p.StatusValue); break;
+                case TDLSortState.CreatedDesc: tdlContext = tdlContext.OrderByDescending(p => p.Created); break;
+            }
+            int recsCount = await tdlContext.CountAsync();
+            var pager = new Pager(recsCount, pg, pageSize);
+            int recSkip = (pg - 1) * pageSize;
+            var data = await tdlContext
+                .Skip(recSkip)
+                .Take(pageSize)
+                .ToListAsync();
+            this.ViewBag.Pager = pager;
+            ViewBag.NameSort = sortState == TDLSortState.NameAsc ? TDLSortState.NameDesc : TDLSortState.NameAsc;
+            ViewBag.PriorityValueSort = sortState == TDLSortState.PriorityValueAsc ? TDLSortState.PriorityValueDesc : TDLSortState.PriorityValueAsc;
+            ViewBag.StatusValueSort = sortState == TDLSortState.StatusValueAsc ? TDLSortState.StatusValueDesc : TDLSortState.StatusValueAsc;
+            ViewBag.CreatedSort = sortState == TDLSortState.CreatedAsc ? TDLSortState.CreatedDesc : TDLSortState.CreatedAsc;
+            return View(data);
+        }
+        [Authorize]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+                return NotFound();
+            var goal = await _context.Goals.FirstOrDefaultAsync(m => m.Id == id);
+            if (goal == null)
+                return NotFound();
+            return View(goal);
+        }
+        [Authorize]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Name,Description,Status,Priority,UserName")] Goal goal)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    goal.Created = DateTime.Now;
+                    _context.Add(goal);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (Exception ex){ ModelState.AddModelError("Status", ex.Message);}
+            }
+
+            return View(goal);
+        }
+        [Authorize]
+        public async Task<IActionResult> Edit(int? id)
+        {
+           
+            if (id == null)
+                return NotFound();
+            var goal = await _context.Goals.FindAsync(id);
+            if (goal == null)
+                return NotFound();
+            return View(goal);
+        }
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Priority,Description,Status,UserName")] Goal goal)
+        {
+           
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var existingGoal = await _context.Goals.FindAsync(id);
+                    if (existingGoal == null)
+                        return NotFound();
+                    if (existingGoal.Status == "В процессе" || existingGoal.Status == "Завершена")
+                    {
+                        ViewData["StatusError"] = "Нельзя редактировать задачу в данном состоянии.";
+                        return View(goal);
+                    }
+                    if (goal.Status != null)
+                    {
+                        existingGoal.Name = goal.Name;
+                        existingGoal.Priority = goal.Priority;
+                        existingGoal.Description = goal.Description;
+                        existingGoal.UserName = goal.UserName;
+                        _context.Update(existingGoal);
+                        await _context.SaveChangesAsync();
+                        return RedirectToAction(nameof(Index));
+                    }
+                    else
+                        ViewData["StatusErrors"] = "Неверное состояние задачи.";
+                }catch (ArgumentException ex) { ModelState.AddModelError("Status", ex.Message); }
+            }
+            return View(goal);
+        }
+        [Authorize]
+        public async Task<IActionResult> Delete(int? id)
+        {
+           
+            if (id == null)
+                return NotFound();
+            var goal = await _context.Goals.FirstOrDefaultAsync(m => m.Id == id);
+            if (goal == null)
+                return NotFound();
+            return View(goal);
+        }
+        [Authorize]
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+           
+            var goal = await _context.Goals.FindAsync(id);
+            if (goal != null)
+            {
+                try
+                {
+                    if (goal.Status == "Новая" || goal.Status == "Завершена")
+                    {
+                        _context.Goals.Remove(goal);
+                        await _context.SaveChangesAsync();
+                    }
+                    else
+                    {
+                        ViewData["StatusError"] = "Нельзя удалить задачу с данным статусом.";
+                        return View(goal);
+                    }
+                }
+                catch (Exception ex){ ModelState.AddModelError("Status", ex.Message);  return View(goal);}
+            }
+            return RedirectToAction(nameof(Index));
+        }
+        [Authorize]
+        private bool GoalExists(int id)
+        {
+            return _context.Goals.Any(e => e.Id == id);
+        }
+
+
+
+        [Authorize]
+        public async Task<IActionResult> Start(int? id)
+        {
+           
+            if (id == null)
+                return NotFound();
+            var goal = await _context.Goals.FindAsync(id);
+            if (goal == null)
+                return NotFound();
+            try
+            {
+                if (goal.Status == "Новая")
+                {
+                    goal.Started = DateTime.Now;
+                    goal.Status = "В процессе";
+                    _context.Update(goal);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    ViewData["StatusError1"] = "Нельзя начать выполнение задачи в данном состоянии.";
+                    return View(nameof(Index), await _context.Goals.ToListAsync());
+                }
+            }
+            catch
+            {
+                ViewData["StatusError2"] = "Произошла ошибка при запуске задачи.";
+                return View(nameof(Index), await _context.Goals.ToListAsync());
+            }
+        }
+        [Authorize]
+        public async Task<IActionResult> Complete(int? id)
+        {
+            if (id == null)
+                return NotFound();
+            var goal = await _context.Goals.FindAsync(id);
+            if (goal == null)
+                return NotFound();
+            try
+            {
+                if (goal.Status == "В процессе")
+                {
+                    goal.Ended = DateTime.Now;
+                    goal.Status = "Завершена";
+                    _context.Update(goal);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    ViewData["StatusError3"] = "Нельзя завершить задачу в данном состоянии.";
+                    return View(nameof(Index), await _context.Goals.ToListAsync());
+                }
+            }
+            catch
+            {
+                ViewData["StatusError4"] = "Произошла ошибка при завершении задачи.";
+                return View(nameof(Index), await _context.Goals.ToListAsync());
+            }
+        }
+        public IActionResult ChangeCulture(string culture)
+        {
+            var cultureInfo = new CultureInfo(culture);
+            var requestCulture = new RequestCulture(cultureInfo);
+            var cookieValue = CookieRequestCultureProvider.MakeCookieValue(requestCulture);
+
+            Response.Cookies.Append(
+                CookieRequestCultureProvider.DefaultCookieName,
+                cookieValue,
+                new CookieOptions{  Expires = DateTimeOffset.UtcNow.AddYears(1)}
+            );
+            return Redirect(Request.Headers["Referer"].ToString() ?? "/");
+        }
+
+    }
+}
