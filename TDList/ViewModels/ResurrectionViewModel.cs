efafb2529f5dc63e0fe9bd02bb3dd1bc4ccb9b37
mak7on22 <mur-mur-0998@mail.ru>
2023-11-24 13:36:24 +0600

    added a new view to record the email and send it to the post request method

---
+++ TDList/ViewModels/ResurrectionViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TDList.ViewModels
+{
+    public class ResurrectionViewModel
+    {
+            public string Email { get; set; }  
+    }
+}
